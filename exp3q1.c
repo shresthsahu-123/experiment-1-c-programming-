8e0debf798f33577efa3c7c8bd9f0610aaa86d86
shresthsahu-123 <shresthalln@gmail.com>
2025-09-22 14:46:20 +0530

    Update exp3q1.c

--- exp3q1.c
+++ exp3q1.c
@@ -6,11 +6,9 @@ int main() {
     printf("Enter the sides of triangle: ");
     scanf("%f %f %f", &a, &b, &c);
 
-    // Check triangle validity
     if ((a + b > c) && (b + c > a) && (a + c > b)) {
         printf("The triangle is valid.\n");
 
-        // Check triangle type
         if (a == b && b == c)
             printf("The triangle is equilateral.\n");
         else if (a == b || b == c || a == c)
@@ -18,7 +16,6 @@ int main() {
         else
             printf("The triangle is scalene.\n");
 
-        // Check right angle using Pythagoras theorem
         if ((a*a + b*b == c*c) || (b*b + c*c == a*a) || (a*a + c*c == b*b))
             printf("The triangle is right-angled.\n");
     } else {
